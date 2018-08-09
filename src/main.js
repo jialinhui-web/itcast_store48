1e94dc8c3042a444c27ad58281d38611ed98187a
goddlts <54337467@qq.com>
2018-08-09 10:57:41 +0800

    登录页面

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import router from './router';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
+import '@/assets/css/index.css';
+
 // 注册插件
 Vue.use(ElementUI);
 
