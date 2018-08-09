2eddb0901d326a2b2571cadf56afa6760b60369b
goddlts <54337467@qq.com>
2018-08-09 14:12:47 +0800

    Home的路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 // 导入组件，所有的import 都应该在文件最上面
 import Login from '@/views/Login';
+import Home from '@/views/Home';
 
 // @ 代表的是src的绝对路径
 // @ 是在build/webpack.base.conf.js
@@ -14,6 +15,7 @@ Vue.use(Router);
 
 export default new Router({
   routes: [
+    { name: 'Home', path: '/', component: Home },
     { name: 'Login', path: '/login', component: Login }
   ]
 });
