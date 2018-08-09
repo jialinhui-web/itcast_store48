0702c2ae25c148a0505d593fa0dea69ee87966de
goddlts <54337467@qq.com>
2018-08-09 16:45:56 +0800

    基本表格的演示

--- src/views/users/Users.vue
+++ src/views/users/Users.vue
@@ -15,12 +15,62 @@
         <el-button type="success" plain>成功按钮</el-button>
       </el-col>
     </el-row>
+    <!-- 表格 -->
+    <!-- border 加边框
+      stripe 斑马纹
+      序号列
+    -->
+    <el-table
+      border
+      stripe
+      :data="tableData"
+      style="width: 100%">
+      <!-- 序号 -->
+      <el-table-column
+        type="index"
+        width="50">
+      </el-table-column>
+      <el-table-column
+        prop="date"
+        label="日期"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+    </el-table>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
+    }
+  }
 };
 </script>
 
