93cc3b40c0cda3d95e7a8b2cf48ebe1b15ba1c9f
goddlts <54337467@qq.com>
2018-08-09 17:01:28 +0800

    用户列表展示

--- src/views/users/Users.vue
+++ src/views/users/Users.vue
@@ -23,7 +23,7 @@
     <el-table
       border
       stripe
-      :data="tableData"
+      :data="data"
       style="width: 100%">
       <!-- 序号 -->
       <el-table-column
@@ -31,44 +31,65 @@
         width="50">
       </el-table-column>
       <el-table-column
-        prop="date"
-        label="日期"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        prop="name"
+        prop="username"
         label="姓名"
         width="180">
       </el-table-column>
       <el-table-column
-        prop="address"
-        label="地址">
+        prop="email"
+        label="邮箱"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="mobile"
+        label="电话"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="create_time"
+        label="时间"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="mg_state"
+        label="用户状态"
+        width="80">
+      </el-table-column>
+      <el-table-column
+        label="操作">
       </el-table-column>
     </el-table>
   </el-card>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   data() {
     return {
-      tableData: [{
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1517 弄'
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1519 弄'
-      }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄'
-      }]
+      data: []
+    };
+  },
+  created() {
+    this.loadData();
+  },
+  methods: {
+    // 1 发送请求，获取用户列表
+    async loadData() {
+
+      // 发送请求的时候，要在请求头中添加Authorization=token
+      var token = sessionStorage.getItem('token');
+      axios.defaults.headers.common['Authorization'] = token;
+
+      var response = await axios.get('http://localhost:8888/api/private/v1/users?pagenum=1&pagesize=10');
+
+      // response => { data: { data: { users: [] }, meta: { status: [] } }}
+      var { meta: { status, msg } } = response.data;
+      if (status === 200) {
+        this.data = response.data.data.users;
+      } else {
+        this.$message.error(msg);
+      }
     }
   }
 };
