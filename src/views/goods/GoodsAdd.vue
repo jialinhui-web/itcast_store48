104aed9e9918d94ec74a5519a7f2feaaeb5c4b28
goddlts <54337467@qq.com>
2018-08-13 17:16:39 +0800

    添加商品-界面

--- src/views/goods/GoodsAdd.vue
+++ src/views/goods/GoodsAdd.vue
@@ -1,6 +1,16 @@
 <template>
   <el-card class="card">
-    add
+    <!-- 面包屑 -->
+    <my-breadcrumb level1="商品管理" level2="商品列表"></my-breadcrumb>
+
+     <el-alert
+      class="alert"
+      title="添加商品信息"
+      :closable="false"
+      type="info"
+      center
+      show-icon>
+    </el-alert>
   </el-card>
 </template>
 
@@ -11,5 +21,8 @@ export default {
 </script>
 
 <style>
-
+.alert {
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
 </style>
