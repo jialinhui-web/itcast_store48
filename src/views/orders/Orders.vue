da8b7910e993b890653057d296316dfefface0f8
goddlts <54337467@qq.com>
2018-08-16 10:45:51 +0800

    订单列表-界面

--- src/views/orders/Orders.vue
+++ src/views/orders/Orders.vue
@@ -1,12 +1,62 @@
 <template>
   <el-card class="card">
-    orders
+    <my-breadcrumb level1="订单管理" level2="订单列表"></my-breadcrumb>
+    <el-table
+      border
+      stripe
+      :data="data"
+      style="width: 100%; margin-top: 10px">
+      <el-table-column
+        type="index"
+        width="60">
+      </el-table-column>
+      <el-table-column
+        prop=""
+        label="订单编号"
+        width="250">
+      </el-table-column>
+      <el-table-column
+        prop=""
+        label="订单价格"
+        width="80">
+      </el-table-column>
+      <el-table-column
+        prop=""
+        label="是否付款"
+        width="80">
+      </el-table-column>
+      <el-table-column
+        prop=""
+        label="是否发货"
+        width="80">
+      </el-table-column>
+      <el-table-column
+        prop=""
+        label="下单时间"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        label="操作">
+        <template slot-scope="scope">
+          <el-button
+            type="primary"
+            icon="el-icon-edit"
+            size="mini"
+            plain>
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      data: []
+    };
+  }
 };
 </script>
 
