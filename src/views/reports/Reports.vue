083d05c44c50fbcac3cd123cb885ae3e69619573
goddlts <54337467@qq.com>
2018-08-16 14:45:27 +0800

    echarts演示

--- src/views/reports/Reports.vue
+++ src/views/reports/Reports.vue
@@ -1,12 +1,42 @@
 <template>
   <el-card class="card">
     <my-breadcrumb level1="数据统计" level2="数据报表"></my-breadcrumb>
+
+    <div ref="chart" style="width: 800px; height: 600px; margin-top: 20px"></div>
   </el-card>
 </template>
 
 <script>
+import echarts from 'echarts';
 export default {
-
+  mounted() {
+    this.init();
+  },
+  methods: {
+    init() {
+      // 初始化echarts
+      const myChart = echarts.init(this.$refs.chart);
+      const option = {
+        title: {
+            text: 'ECharts 入门示例'
+          },
+          tooltip: {},
+          legend: {
+            data:['销量']
+          },
+          xAxis: {
+            data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+          },
+          yAxis: {},
+          series: [{
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+          }]
+      };
+      myChart.setOption(option);
+    }
+  }
 };
 </script>
 
